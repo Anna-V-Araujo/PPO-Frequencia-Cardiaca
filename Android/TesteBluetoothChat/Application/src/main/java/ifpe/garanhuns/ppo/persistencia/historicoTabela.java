3237345feb43a7f3a5e460d4712f3fefbca2f92e
Anna Vitória <annavitoria14@gmail.com>
2016-12-20 16:00:31 -0300

    Alterações na tabela - Não vai

--- Android/TesteBluetoothChat/Application/src/main/java/ifpe/garanhuns/ppo/persistencia/historicoTabela.java
+++ Android/TesteBluetoothChat/Application/src/main/java/ifpe/garanhuns/ppo/persistencia/historicoTabela.java
@@ -2,14 +2,20 @@ package ifpe.garanhuns.ppo.persistencia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 import com.example.android.bluetoothchat.R;
 
 public class historicoTabela extends AppCompatActivity {
 
+    private GridView gridTabela;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico_tabela);
+        gridTabela = (GridView)findViewById(R.id.tabela);
     }
+
+
 }
